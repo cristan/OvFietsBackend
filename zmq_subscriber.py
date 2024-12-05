3cf9821f3e5fe469fa569482389741d5173559cd
Cristan Meijer <cristan.meijer@gmail.com>
2024-12-05 22:28:53 +0100

    Retry if we somehow lose connection

--- zmq_subscriber.py
+++ zmq_subscriber.py
@@ -6,21 +6,6 @@ import time
 import threading
 from google.cloud import storage
 
-# Create a ZeroMQ context
-context = zmq.Context()
-
-# Create a SUB (subscriber) socket
-socket = context.socket(zmq.SUB)
-
-# Connect to the server
-socket.connect("tcp://vid.openov.nl:6703")
-
-# Subscribe to the /OVfiets topic
-topic = "/OVfiets"
-socket.setsockopt_string(zmq.SUBSCRIBE, topic)
-
-print(f"Subscribed to topic: {topic}")
-
 # Directory to store the final JSON
 base_directory = "OVfiets"
 os.makedirs(base_directory, exist_ok=True)
@@ -28,11 +13,20 @@ os.makedirs(base_directory, exist_ok=True)
 # A dictionary to accumulate the data
 combined_data = {}
 
+def create_socket(context):
+    """
+    Creates and returns a configured ZeroMQ SUB socket.
+    """
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://vid.openov.nl:6703")
+    topic = "/OVfiets"
+    print(f"Subscribing to topic {topic}")
+    socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+    return socket
+
 def write_combined_json():
     file_path = os.path.join(base_directory, "combined_data.json")
-
     to_write = list(combined_data.values())
-
     with open(file_path, 'w', encoding='utf-8') as json_file:
         json.dump(to_write, json_file, ensure_ascii=False)
     print(f"Combined JSON saved to {file_path}")
@@ -64,7 +58,6 @@ def filter_old_entries():
         del combined_data[key]
         print(f"Removed old location data: {key}")
 
-write_timer = None
 
 def save_and_upload():
     write_combined_json()
@@ -72,83 +65,70 @@ def save_and_upload():
     write_timer = None
 
 def save_and_upload_delayed():
-    # Make write_timer global so we can overwrite it.
     global write_timer
-
-    # Cancel the current timer if it's already running
     if write_timer is not None:
         write_timer.cancel()
-
-    # Start a new timer for 1 second
     write_timer = threading.Timer(1.0, save_and_upload)
     write_timer.start()
 
-# Only return the fields useful for the overview call
 def get_useful_data(entry):
-    useful = {}
-    # strip because Duiven has a space at the end of its description.
-    useful['description'] = entry.get('description').strip()
-    useful['stationCode'] = entry.get('stationCode')
-    useful['lat'] = entry.get('lat')
-    useful['lng'] = entry.get('lng')
-    useful['link'] = {}
-    useful['link']['uri'] = entry['link']['uri']
-    useful['extra'] = {}
-    useful['extra']['locationCode'] = entry['extra']['locationCode']
-    useful['extra']['fetchTime'] = entry['extra']['fetchTime']
-    useful['extra']['rentalBikes'] = entry['extra'].get('rentalBikes', None)
-    useful['openingHours'] = entry.get('openingHours')
-    return useful
+    return {
+        # strip because Duiven has a space at the end of its description.
+        'description': entry.get('description', '').strip(),
+        'stationCode': entry.get('stationCode'),
+        'lat': entry.get('lat'),
+        'lng': entry.get('lng'),
+        'link': {'uri': entry['link']['uri']},
+        'extra': {
+            'locationCode': entry['extra']['locationCode'],
+            'fetchTime': entry['extra']['fetchTime'],
+            'rentalBikes': entry['extra'].get('rentalBikes', None)
+        },
+        'openingHours': entry.get('openingHours')
+    }
 
-# Loop to handle receiving messages
-while True:
-    try:
-        # Blocking call to receive the first topic (wait for incoming message)
-        topic_received = socket.recv_string()
+def receive_messages(socket):
+    """
+    Handle incoming messages on the given socket and update combined_data.
+    """
+    topic_received = socket.recv_string()
+    while True:
+        try:
+            message = socket.recv(flags=zmq.NOBLOCK)
+            decompressed_message = gzip.decompress(message)
+            message_str = decompressed_message.decode('utf-8')
+            json_data = json.loads(message_str)
+            location_code = topic_received.split("/")[-1]
+            print(f"Received data for {location_code} with fetchTime {json_data['extra']['fetchTime']}")
+            if 'rentalBikes' in json_data['extra']:
+                combined_data[location_code] = get_useful_data(json_data)
+            
+            topic_received = socket.recv_string(flags=zmq.NOBLOCK)
+        except zmq.Again:
+            # No more messages available
+            return
 
-        # Check for available messages in non-blocking mode
-        while True:
-            try:
-                # Non-blocking receive of the actual message data
-                message = socket.recv(flags=zmq.NOBLOCK)
-
-                # Attempt to decompress the message assuming it's gzipped
-                try:
-                    decompressed_message = gzip.decompress(message)
-                    # Decode the decompressed message as UTF-8
-                    message_str = decompressed_message.decode('utf-8')
-
-                    # Try to parse the string as JSON
-                    json_data = json.loads(message_str)
-
-                    # Get the location code from the topic (e.g., eml001 from /OVfiets/EML/eml001)
-                    location_code = topic_received.split("/")[-1]
-
-                    # Log a one-liner when JSON is received
-                    print(f"Received data for {location_code} with fetchTime {json_data['extra']['fetchTime']}")
-                    # print(f"JSON: {json_data}")
-
-                    # Sometimes we don't get the amount of bikes. Don't bother with these entries.
-                    if 'rentalBikes' in json_data['extra']:
-                        combined_data[location_code] = get_useful_data(json_data)
-
-                except (OSError, json.JSONDecodeError):
-                    print(f"Received non-compressed or non-JSON message")
-
-                # After handling the message, receive the next topic
-                topic_received = socket.recv_string(flags=zmq.NOBLOCK)
-
-            except zmq.Again:
-                # No more messages available
-                break
-
-        filter_old_entries()
-        save_and_upload_delayed()
-
-    except KeyboardInterrupt:
-        print("Interrupted")
-        break
-
-# Clean up
-socket.close()
-context.term()
+# Main loop
+try:
+    context = zmq.Context()
+    socket = create_socket(context)
+    while True:
+        try:
+            receive_messages(socket)
+            filter_old_entries()
+            save_and_upload_delayed()
+        except zmq.ZMQError:
+            print("Connection lost. Retrying in 5 minutes.")
+            socket.close()
+            context.term()
+            time.sleep(300) # 5 minutes * 60 = 300 seconds
+            context = zmq.Context()
+            socket = create_socket(context)
+except KeyboardInterrupt:
+    print("Interrupted by user.")
+finally:
+    # Clean up resources
+    if write_timer is not None:
+        write_timer.cancel()
+    socket.close()
+    context.term()
