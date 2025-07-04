c7306f3d95999467ca4bed0a47dd859cf37ce47e
Cristan Meijer <cristan.meijer@gmail.com>
2025-07-04 20:03:18 +0200

    Rename rules

--- firestore.tf
+++ firestore.tf
@@ -13,7 +13,7 @@ resource "google_firestore_field" "hourly_capacity_ttl" {
   ttl_config { }
 }
 
-resource "google_firebaserules_ruleset" "firestore_rules" {
+resource "google_firebaserules_ruleset" "hourly_capacity_public_access" {
   project = var.project_id
   provider    = google-beta
 
@@ -30,8 +30,8 @@ resource "google_firebaserules_ruleset" "firestore_rules" {
 }
 
 // Needs a terraform/tofu import to overwrite the default one. Change imports.tf to get this to work.
-resource "google_firebaserules_release" "firestore_release" {
+resource "google_firebaserules_release" "hourly_capacity_public_access" {
   name         = "cloud.firestore"
   project      = var.project_id
-  ruleset_name = google_firebaserules_ruleset.firestore_rules.name
+  ruleset_name = google_firebaserules_ruleset.hourly_capacity_public_access.name
 }
